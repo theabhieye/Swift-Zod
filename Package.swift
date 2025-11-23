0642e160965f6921736dc00be37e7260509dce62
Abhishek Kapoor <40399665+theabhieye@users.noreply.github.com>
2025-11-23 13:29:25 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,23 +4,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "Swift-Zod",
+    name: "SwiftZod",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Swift-Zod",
-            targets: ["Swift-Zod"]
+            name: "SwiftZod",
+            targets: ["SwiftZod"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Swift-Zod"
+            name: "SwiftZod"
         ),
         .testTarget(
-            name: "Swift-ZodTests",
-            dependencies: ["Swift-Zod"]
+            name: "SwiftZodTests",
+            dependencies: ["SwiftZod"]
         ),
     ]
 )
